99aceae7df7d1274c74167cd933b3b835efd78d4
Keerthi raja <105999990+22Raja@users.noreply.github.com>
2025-05-03 15:00:13 +0530

    With Dummy Node and Without Dummy Node

--- LinkList/Remove Nth Node From End of List.py
+++ LinkList/Remove Nth Node From End of List.py
@@ -3,20 +3,23 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# With Dummy Node.
 class Solution(object):
     def removeNthFromEnd(self, head, n):
         dummyNode = ListNode(0,head)
-        l = dummyNode
-        r = head   
-
-        while n > 0 and  r :
-            r = r.next 
+        slow = dummyNode
+        fast = head   
+        # Move fast pointer n steps ahead
+        while n > 0 and fast:
+            fast = fast.next 
             n -= 1 
-        while r:
-            l = l.next
-            r = r.next 
+        # Move both pointers until fast reaches the end
+        while fast:
+            slow = slow.next
+            fast = fast.next 
+        # Remove the nth node from the end
 
-        l.next = l.next.next
+        slow.next = slow.next.next
 
         return dummyNode.next 
     
@@ -24,5 +27,34 @@ class Solution(object):
 # space complexity - O(1)
 
 
+# Without Dummy Node.
+ def removeNthFromEnd(self, head, n):
+        if not head: 
+            return None 
+        length = 0 
+        current = head 
+        while current: 
+            length += 1 
+            current = current.next
+        # Removing the first node when n th is equal length
+
+        if n == length:
+            start_node = head.next
+            head.next = None 
+            return start_node 
+        #Find the node before the one to delete
+        before_node = head  
+        for _ in range(length-n-1): 
+            before_node = before_node.next
+
+        # Remove the nth node from the end
+        delete_node = before_node.next 
+        Another_node = delete_node.next 
+        before_node.next = Another_node
+
+        return head
+     
+#Time Complexity: O(n)
+#Space Complexity: O(1)
 
 #https://leetcode.com/problems/remove-nth-node-from-end-of-list/
