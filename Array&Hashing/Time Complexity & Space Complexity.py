bc689175592c9522ff0b0df51aab302118b32ad7
Keerthi raja <105999990+22Raja@users.noreply.github.com>
2025-05-19 17:46:18 +0530

    Time Complexity & Space Complexity

---
+++ Array&Hashing/Time Complexity & Space Complexity.py
@@ -0,0 +1,11 @@
+# Hash Set      # Time Complexity     # Space Complexity
+# Add(add)    -        O(1)                    O(n) 
+# check(in)   -        O(1)                    O(n)  
+# Remove(discard) -    O(1)                    O(n)  
+
+
+# Hash Map      # Time Complexity     # Space Complexity  
+# Insert(put)   -    O(1)                 O(n)     
+# Get(get)       -   O(1)                 O(n) 
+# Remove(delete) -   O(1)                 O(n)
+# lookup key     -   O(1)                 o(n) 
